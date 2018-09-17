3f75e394895a42fdcfdc2b9a175d130014b259c5
Di Liu <unknowngas@gmail.com>
2018-09-17 11:22:05 -0400

    Fix undefined case of resList

--- routes/start.js
+++ routes/start.js
@@ -10,7 +10,7 @@ var iot = require('../lib/iot.js');
 const {transports, createLogger, format} = require('winston');
 
 //const discoveryFrequency = '*/30 * * * * *';
-const discoveryFrequency = '*/5 * * * *';
+const discoveryFrequency = '0 */8 * * *';
 const scanningFrequency = '*/30 * * * * *';
 const minerPort = 80;
 const minerUser = 'root';
